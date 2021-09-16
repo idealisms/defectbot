a5ad2aa6d99b58a27bdffdb2033d0d8548cd5977
tony chang <tony@ponderer.org>
2021-09-16 14:16:07 -0700

    Run prettier on calc*

--- commands/calc.js
+++ commands/calc.js
@@ -1,50 +1,39 @@
 function tokenize(inputStr) {
-    // This regex creates tokens out of numbers, open paren, close paren, plus,
-    // times, divide, and subtract.
-    const results = inputStr.match(/(\d|[.])+|[(]|[)]|[+]|[*]|[\/]|[-]/gm);
-    if (results === null) {
-        return [];
+  // This regex creates tokens out of numbers, open paren, close paren, plus,
+  // times, divide, and subtract.
+  const results = inputStr.match(/(\d|[.])+|[(]|[)]|[+]|[*]|[\/]|[-]/gm);
+  if (results === null) {
+    return [];
+  }
+
+  return results.filter((token) => {
+    if ("()+-*/".indexOf(token) != -1) {
+      return true;
     }
-
-    return results.filter((token) => {
-        if ('()+-*/'.indexOf(token) != -1) {
-            return true;
-        }
-        return isFinite(token);
-    });
+    return isFinite(token);
+  });
 }
 
-function shuntingYard(tokens) {
-
+function shouldPop(operator_stack, op1) {
+  const op2 = operator_stack[operator_stack.length - 1];
+  if (!op2 || op2 == "(") {
+    return;
+  }
 }
 
-function evalRPN(operationStack) {
+// https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+function shuntingYard(tokens) {}
 
-}
+function evalRPN(operationStack) {}
 
 function calc(inputStr, say) {
-    const tokens = tokenize(inputStr);
-    const operationStack = shuntingYard(tokens);
-    const results = evalRPN(operationStack);
-    if (results) {
-        say(results);
-    }
-}
-
-function doCalc(channel) {
-    const tokens = /(\d|[.])+|[(]|[)]|[+]|[*]|[\/]|[-]/gm;
-    // Shunting-yard https://en.wikipedia.org/wiki/Shunting-yard_algorithm
-    const output_queue = [];  // append only
-    const operator_stack = [];  // top of the stack is the end
-    for (const token of tokens) {
-      if (Number.isFinite(Number.parseFloat(token))) {
-        
-      }
-    }
-  
-    // RPN eval
+  const tokens = tokenize(inputStr);
+  const operationStack = shuntingYard(tokens);
+  const results = evalRPN(operationStack);
+  if (results) {
+    say(results);
   }
-  
+}
 
 calc.tokenizeForTest = tokenize;
 calc.shuntingYardForTest = shuntingYard;
--- commands/calc.test.js
+++ commands/calc.test.js
@@ -1,14 +1,27 @@
-const calc = require('./calc');
+const calc = require("./calc");
 
-test('tokenize', () => {
-    expect(calc.tokenizeForTest('1+1')).toEqual(['1', '+', '1']);
-    expect(calc.tokenizeForTest('1 + 1')).toEqual(['1', '+', '1']);
-    expect(calc.tokenizeForTest('foo')).toEqual([]);
-    expect(calc.tokenizeForTest('System.eval')).toEqual([]);
-    expect(calc.tokenizeForTest('1..2 ..2 2.2 3..')).toEqual(['2.2']);
-    expect(calc.tokenizeForTest('1.0-+1')).toEqual(['1.0', '-', '+', '1']);
-    expect(calc.tokenizeForTest('(8+1.5 * (4+4+4) )*.75')).toEqual(
-        ['(', '8', '+', '1.5', '*', '(', '4', '+', '4', '+', '4', ')', ')',
-         '*', '.75']);
+test("tokenize", () => {
+  expect(calc.tokenizeForTest("1+1")).toEqual(["1", "+", "1"]);
+  expect(calc.tokenizeForTest("1 + 1")).toEqual(["1", "+", "1"]);
+  expect(calc.tokenizeForTest("foo")).toEqual([]);
+  expect(calc.tokenizeForTest("System.eval")).toEqual([]);
+  expect(calc.tokenizeForTest("1..2 ..2 2.2 3..")).toEqual(["2.2"]);
+  expect(calc.tokenizeForTest("1.0-+1")).toEqual(["1.0", "-", "+", "1"]);
+  expect(calc.tokenizeForTest("(8+1.5 * (4+4+4) )*.75")).toEqual([
+    "(",
+    "8",
+    "+",
+    "1.5",
+    "*",
+    "(",
+    "4",
+    "+",
+    "4",
+    "+",
+    "4",
+    ")",
+    ")",
+    "*",
+    ".75",
+  ]);
 });
-
