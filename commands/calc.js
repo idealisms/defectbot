8661a2af21ef25ec1bf19a28d303d55a9437e657
tony chang <tony@ponderer.org>
2021-09-16 14:36:52 -0700

    Implement shunting yard.

--- commands/calc.js
+++ commands/calc.js
@@ -15,14 +15,63 @@ function tokenize(inputStr) {
 }
 
 function shouldPop(operator_stack, op1) {
+  const precedence = {
+    "+": 2,
+    "-": 2,
+    "*": 3,
+    "/": 3,
+  };
   const op2 = operator_stack[operator_stack.length - 1];
   if (!op2 || op2 == "(") {
-    return;
+    return false;
   }
+  return (
+    precedence[op2] > precedence[op1] || precedence[op2] == precedence[op1]
+  );
 }
 
 // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
-function shuntingYard(tokens) {}
+function shuntingYard(tokens) {
+  const output_queue = []; // append only
+  const operator_stack = []; // top of the stack is the end
+
+  for (const token of tokens) {
+    if (Number.isFinite(Number.parseFloat(token))) {
+      output_queue.push(Number.parseFloat(token));
+    } else if ("+-*/".indexOf(token) != -1) {
+      while (shouldPop(operator_stack, token)) {
+        output_queue.push(operator_stack.pop());
+      }
+      operator_stack.push(token);
+    } else if (token == "(") {
+      operator_stack.push(token);
+    } else if (token == ")") {
+      while (operator_stack[operator_stack.length - 1] != "(") {
+        if (operator_stack.length == 0) {
+          console.log("Mismatched parentheses.");
+          return [];
+        }
+        output_queue.push(operator_stack.pop());
+      }
+      if (operator_stack.pop() != "(") {
+        console.log("Should not be here. Missing open paren.");
+        return [];
+      }
+    } else {
+      console.log("Unknown token.");
+      return [];
+    }
+  }
+  while (operator_stack.length > 0) {
+    const op = operator_stack.pop();
+    if (op == "(") {
+      console.log("Mismatched parentheses.");
+      return [];
+    }
+    output_queue.push(op);
+  }
+  return output_queue;
+}
 
 function evalRPN(operationStack) {}
 
