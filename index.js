30f52057316aa4cc12b7557248a3ed619aa40f2c
tony chang <tony@ponderer.org>
2021-11-17 22:01:22 -0800

    Run through prettier.

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const tmi = require('tmi.js');
-const secrets = require('secrets');
-const commands = require('./commands');
+const tmi = require("tmi.js");
+const secrets = require("secrets");
+const commands = require("./commands");
 
 // Define configuration options
 const opts = {
   identity: {
     username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN
+    password: process.env.OAUTH_TOKEN,
   },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
+  channels: [process.env.CHANNEL_NAME],
 };
 
 // Create a client with our options
 const client = new tmi.client(opts);
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on("message", onMessageHandler);
+client.on("connected", onConnectedHandler);
 
 // Connect to Twitch IRC.
 client.connect();
 
 // Called every time a message comes in
 function onMessageHandler(channel, tags, msg, isSelf) {
-  if (isSelf || !msg.startsWith('!')) { return; }
+  if (isSelf || !msg.startsWith("!")) {
+    return;
+  }
 
-  const commandName = msg.split(' ', 1)[0];
+  const commandName = msg.split(" ", 1)[0];
   const commandInput = msg.slice(commandName.length + 1);
   const say = (txt) => client.say(channel, txt);
 
   // If the command is known, let's execute it
-  if (commandName === '!dice') {
+  if (commandName === "!dice") {
     commands.dice(commandInput, say);
-  } else if (commandName === '!calc') {
-    commands.calc(commandInput, say)
+  } else if (commandName === "!calc") {
+    commands.calc(commandInput, say);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
@@ -44,5 +44,5 @@ function onMessageHandler(channel, tags, msg, isSelf) {
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
-  client.say(process.env.CHANNEL_NAME, 'I am online.');
+  client.say(process.env.CHANNEL_NAME, "I am online.");
 }
