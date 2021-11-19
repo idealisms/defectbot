8cd0eeb85d6249d348200b87a8334f94434a666b
tony chang <tony@ponderer.org>
2021-11-18 20:46:26 -0800

    Serve audio files via the webserver.
    These are the playable audio files.

---
+++ sounds/README.md
@@ -0,0 +1,5 @@
+Put audio files in this directory. They can be
+played from chat by using `!filename` (lowercase
+and without the file extension). For example, if
+`Giraffe.wav` was in this directory, it could be
+played with `!giraffe`.
--- webserver.js
+++ webserver.js
@@ -3,11 +3,31 @@ const fs = require("fs");
 const scramjet = require("scramjet");
 
 function createWebServer(opts) {
+  // A set of all the filenames.
+  const soundFilenames = new Set();
+  // Map command names to the sound file name.
+  const soundFilesMap = new Map();
+
+  fs.readdir("./sounds", (err, files) => {
+    for (const filename of files) {
+      if (
+        filename.toLowerCase().endsWith(".wav") ||
+        filename.toLowerCase().endsWith(".mp3")
+      ) {
+        soundFilenames.add(filename);
+        soundFilesMap.set(filename.substr(0, filename.length - 4), filename);
+      }
+    }
+  });
+
   const requestListener = (req, res) => {
     console.log(req.method, req.url);
+    if (req.method != "GET") {
+      return;
+    }
+
     if (req.url == "/favicon.ico") {
-      res.writeHead(200);
-      res.end();
+      res.writeHead(200).end();
     } else if (req.url == "/client.js") {
       res.writeHead(200, { "Content-Type": "text/javascript" });
       fs.createReadStream("client.js")
@@ -18,11 +38,24 @@ function createWebServer(opts) {
         )
         .join("\n")
         .pipe(res);
+    } else if (req.url.startsWith("/sounds/")) {
+      const soundFilename = req.url.split("/", 3)[2];
+      if (soundFilenames.has(soundFilename)) {
+        res.writeHead(200, {
+          "Content-Type": `audio/${
+            soundFilename.toLowerCase().endsWith("wav") ? "wav" : "mpeg"
+          }`,
+        });
+        fs.createReadStream(`sounds/${soundFilename}`).pipe(res);
+      } else {
+        res.writeHead(404).end();
+      }
     } else {
-      res.writeHead(200);
-      res.write(
-        '<html><head><script src="/client.js"></script></head><body></body></html>'
-      );
+      res
+        .writeHead(200)
+        .write(
+          '<html><head><script src="/client.js"></script></head><body></body></html>'
+        );
       res.end();
     }
   };
