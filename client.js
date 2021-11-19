3e4b21c7dba60f2e6a24cbcd2509a5ec22f7968a
tony chang <tony@ponderer.org>
2021-11-19 09:30:22 -0800

    Add reconnect to client.
    From https://stackoverflow.com/a/52858704

--- client.js
+++ client.js
@@ -1,17 +1,27 @@
 // This is the client side JS. It will be executed
 // in the browser (OBS overlay).
-const ws = new WebSocket("ws://HOSTNAME:PORT/");
-ws.onopen = function () {
-  console.log("WebSocket Client Connected");
-  // Should we send a keep alive?
-};
-ws.onmessage = function (event) {
-  console.log("Received: '" + event.data + "'");
-  const message = JSON.parse(event.data);
-  if (message.cmd == "play") {
-    var sound = new Howl({
-      src: [message.filename],
-    });
-    sound.play();
-  }
-};
+function startWebsocket() {
+  var ws = new WebSocket("ws://HOSTNAME:PORT");
+
+  ws.onopen = (event) => {
+    console.log("WebSocket Client Connected");
+  };
+
+  ws.onmessage = (event) => {
+    console.log("Received: '" + event.data + "'");
+    const message = JSON.parse(event.data);
+    if (message.cmd == "play") {
+      var sound = new Howl({
+        src: [message.filename],
+      });
+      sound.play();
+    }
+  };
+
+  ws.onclose = (event) => {
+    console.log("Connection closed");
+    ws = null;
+    setTimeout(startWebsocket, 5000);
+  };
+}
+startWebsocket();
