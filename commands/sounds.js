7cd9238d1ca7d5b76e804e76cf9097b899b9f3b8
tony chang <tony@ponderer.org>
2022-05-21 13:52:14 -0700

    Remove !listsounds1 and !listsounds2.
    There are too many sounds. Instead link to an external
    file with the sounds.

--- commands/sounds.js
+++ commands/sounds.js
@@ -3,35 +3,13 @@ class Sounds {
     this.client = client;
     this.soundFilesMap = soundFilesMap;
     this.webSocketServer = webSocketServer;
-
-    this.commandsArray = Array.from(soundFilesMap.keys())
-      .sort()
-      .map((commandName) => `!${commandName}`);
   }
 
   handle(channel, tags, commandName, commandInput) {
     if (commandName === "!listsounds") {
       this.client.say(
         channel,
-        "Use !listsounds1 and !listsounds2 to see a list of Super Auto Pets sounds I know."
-      );
-      return true;
-    }
-    if (commandName === "!listsounds1") {
-      this.client.say(
-        channel,
-        `I know the following sounds: ${this.commandsArray
-          .slice(0, this.commandsArray.length / 2)
-          .join(" ")}`
-      );
-      return true;
-    }
-    if (commandName === "!listsounds2") {
-      this.client.say(
-        channel,
-        `I know the following sounds: ${this.commandsArray
-          .slice(this.commandsArray.length / 2)
-          .join(" ")}`
+        "I know the following sounds: https://raw.githubusercontent.com/idealisms/defectbot/main/sounds/list%20of%20animal%20sounds.txt"
       );
       return true;
     }
